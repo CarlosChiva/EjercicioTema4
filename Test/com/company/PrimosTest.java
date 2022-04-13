1880445567cc85fee529ee3ba4522a39c0fe5888
Carlos Chiva <Aluchi1821@ieselcaminas.org>
2022-04-13 18:28:24 +0200

    Tercera prueba de refactorizacion, comprobar tests de metodos individuales

--- Test/com/company/PrimosTest.java
+++ Test/com/company/PrimosTest.java
@@ -5,24 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimosTest {
-Primos primos=new Primos();
-int[] vector;
+    Primos primos = new Primos();
+    int[] vector;
+
     @Test
     void generarPrimos() {
-        int[] numerosPrimoshastaSiete={2,3,5,7};
-        vector=primos.generarPrimos(7);
-        assertArrayEquals(vector,numerosPrimoshastaSiete);
+        int[] numerosPrimoshastaSiete = {2, 3, 5, 7};
+        vector = primos.generarPrimos(7);
+        assertArrayEquals(vector, numerosPrimoshastaSiete);
     }
+
     @Test
     void generarPrimos_menor_de_dos_devuelve_0() {
-        int[] numerosPrimoshasta1=new int[0];
-        vector=primos.generarPrimos(1);
-        assertArrayEquals(vector,numerosPrimoshasta1);
+        int[] numerosPrimoshasta1 = new int[0];
+        vector = primos.generarPrimos(1);
+        assertArrayEquals(vector, numerosPrimoshasta1);
     }
 
     @Test
-    void inicializedArray_boolean_0_1_returnFalse() {
-
+    void longitud_de_array() {
+        int[] numerosPrimoshastaSiete = {2, 3, 5, 7};
+        vector = primos.generarPrimos(7);
+        assertEquals(numerosPrimoshastaSiete.length, vector.length);
 
     }
 }
