292af69b7d6dd059437872e6a9587ca556578b54
Carlos Chiva <Aluchi1821@ieselcaminas.org>
2022-04-13 18:39:32 +0200

    Cuarta prueba de refactorizacion, documentacion

--- src/com/company/Primos.java
+++ src/com/company/Primos.java
@@ -8,6 +8,12 @@ public class Primos {
     static int dim;
     // Tamaño del array
     static boolean[] esPrimo;
+
+    /**
+     * Principal metodo que actua para generar un array con los primos existentes hasta el numero que se introduzca
+     * @param max Numero maximo para el calculo de numeros primos hasta llegar a ese numero
+     * @return Devuelve un array con todos los numeros primos existentes hasta el numero marcado por el parametro de entrada
+     */
     public static int[] generarPrimos(int max) {
          dim = max + 1;
         esPrimo = new boolean[dim];
@@ -21,8 +27,11 @@ public class Primos {
         }
     }
 
+    /**
+     * Metodo el cual rellena el vector principal con numeros primos encontrados
+     * @return Devuelve un array con los numeros primos encontrados
+     */
     private static int[] arrayDePrimos() {
-        // Rellenar el vector de números primos
         int[] primos = new int[numeroDePrimos()];
         for (i = 0, j = 0; i < dim; i++) {
             if (esPrimo[i]) {
@@ -32,6 +41,10 @@ public class Primos {
         return primos;
     }
 
+    /**
+     * Metodo que cuenta cuantos numeros primos van ha haber para determinar la longitud del array del vector
+     * @return devuelve el numero de longitud que tendra el array del vector el cual se rellenara de numeros primos
+     */
     private static int numeroDePrimos() {
         // ¿Cuántos primos hay?
         int cuenta = 0;
@@ -42,6 +55,10 @@ public class Primos {
         }
         return cuenta;
     }
+
+    /**
+     * Metodo que criba para eliminar ciertos numeros los cuales no son primos
+     */
     private static void criba() {
         // Criba
         for (i = 2; i < Math.sqrt(dim) + 1; i++) {
@@ -53,8 +70,12 @@ public class Primos {
             }
         }
     }
+
+    /**
+     * Metodo que inicializa el array principal de numeros enteros y criba el numero 0 y 1 como no primos
+     */
     public static void inicializedArray() {
-        // Inicializar el array
+
         for (i = 0; i < dim; i++) {
             esPrimo[i] = true;
         }
